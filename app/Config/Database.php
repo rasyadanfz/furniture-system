3d3e678f97147f0b54707de4b1eb0eff4d6b527f
rasyadanfz <18221103@std.stei.itb.ac.id>
2023-12-18 11:41:35 +0700

    update docker

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
-        'username'     => '',
-        'password'     => '',
-        'database'     => '',
+        'hostname'     => 'app_db',
+        'username'     => 'root',
+        'password'     => 'root',
+        'database'     => 'furniture_db',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
